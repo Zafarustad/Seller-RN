e6f48aa6187d1a447f80ddc68b2f5efcdc848c47
unknown <zafarustad123@gmail.com>
2021-05-10 10:17:12 +0530

    api-end

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-// const api = 'https://s7cjip52jl.execute-api.ap-south-1.amazonaws.com/production';
-const api = 'https://64bcb955dd4c.ngrok.io';
+const api = 'https://s7cjip52jl.execute-api.ap-south-1.amazonaws.com/production';
+// const api = 'https://64bcb955dd4c.ngrok.io';
 
 export {api};
